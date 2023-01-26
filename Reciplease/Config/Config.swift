f194deef5199acb87764c67236603dbfb6d63844
Lorene Brocourt <lorenebrocourt@gmail.com>
2023-01-26 17:55:15 +0100

    Add Loading & AlertProvider
    Aler Provider:
    Creation of a service in charge of alert messages cases
    Use of this service in controller Search and presenting alert pop-up to user
    
    Gestion of error cases such as :
    - can't request if ingredientList is empty
    presentation of a popup
    - request failed because of wrong ingredients (such as strange caracter's chains)
    
    Loading:
    Creation of a loader in SearchViewController to indicate to user that request is loading

--- Reciplease/Config/Config.swift
+++ Reciplease/Config/Config.swift
@@ -5,8 +5,8 @@
 //  Created by Lorene Brocourt on 13/01/2023.
 //
 struct Config {
-    static let apiKey: String = ""
+    static let apiKey: String = "d938461c89051a47bebc8afd726be449"
     static let recipeUrl: String = "https://api.edamam.com/api/recipes/v2?"
-    static let id: String = ""
+    static let id: String = "c142bf26"
 }
 
